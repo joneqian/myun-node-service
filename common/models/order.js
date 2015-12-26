da16974ba43cfa789395c13dda1a68d13cef2710
qianqing <qian.qing@aliyun.com>
2015-12-26 16:07:39 +0800

    add order model

--- common/models/order.js
+++ common/models/order.js
@@ -4,6 +4,30 @@ module.exports = function (Order) {
     var req = context.req;
     req.body.date = utils.formatTime(new Date());
     req.body.userId = req.accessToken.userId;
-    next();
+    Order.getApp(function (error , app) {
+      if (error) {
+        next(error);
+      } else {
+        app.models.Product.findById(req.body.productId, function (error, product) {
+          if (error) {
+            next(error);
+            return;
+          }
+
+          if (!product) {
+            next(new Error("product isn't exist"));
+            return;
+          }
+
+          if (product.count <= 0) {
+            next(new Error("product have been sold out"));
+            return;
+          }
+
+          next();
+        });
+      }
+
+    });
   });
 };
