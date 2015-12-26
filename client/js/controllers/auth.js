6d9a1b1396d87621915807add48554503d3848d7
qianqing <qian.qing@aliyun.com>
2015-12-26 16:56:19 +0800

    clear session

--- client/js/controllers/auth.js
+++ client/js/controllers/auth.js
@@ -1,37 +1,39 @@
 angular
-    .module('app')
-    .controller('AuthLoginController', ['$scope', 'AuthService', '$state',
-        function ($scope, AuthService, $state) {
-            $scope.user = {
-                email: 'qianqing0@aliyun.com',
-                password: '123'
-            };
+  .module('app')
+  .controller('AuthLoginController', ['$scope', 'AuthService', '$state', '$window', '$rootScope',
+    function ($scope, AuthService, $state, $window, $rootScope) {
+      $rootScope.currentUser = null;
+      $window.sessionStorage.removeItem('currentUser');
+      $scope.user = {
+        email: 'qianqing0@aliyun.com',
+        password: '123'
+      };
 
-            $scope.login = function () {
-                AuthService.login($scope.user.email, $scope.user.password)
-                    .then(function () {
-                        $state.go('my-product');
-                    });
-            };
-        }])
-    .controller('AuthLogoutController', ['$scope', 'AuthService', '$state',
-        function ($scope, AuthService, $state) {
-            AuthService.logout()
-                .then(function () {
-                    $state.go('login');
-                });
-        }])
-    .controller('SignUpController', ['$scope', 'AuthService', '$state',
-        function ($scope, AuthService, $state) {
-            $scope.user = {
-                email: '',
-                password: ''
-            };
+      $scope.login = function () {
+        AuthService.login($scope.user.email, $scope.user.password)
+          .then(function () {
+            $state.go('my-product');
+          });
+      };
+    }])
+  .controller('AuthLogoutController', ['$scope', 'AuthService', '$state',
+    function ($scope, AuthService, $state) {
+      AuthService.logout()
+        .then(function () {
+          $state.go('login');
+        });
+    }])
+  .controller('SignUpController', ['$scope', 'AuthService', '$state',
+    function ($scope, AuthService, $state) {
+      $scope.user = {
+        email: '',
+        password: ''
+      };
 
-            $scope.register = function () {
-                AuthService.register($scope.user.email, $scope.user.password)
-                    .then(function () {
-                        $state.transitionTo('sign-up-success');
-                    });
-            };
-        }]);
+      $scope.register = function () {
+        AuthService.register($scope.user.email, $scope.user.password)
+          .then(function () {
+            $state.transitionTo('sign-up-success');
+          });
+      };
+    }]);
