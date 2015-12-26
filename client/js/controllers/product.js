f2ac747e6fcd10338d1bb58d0486f5b0e3ff1a73
qianqing <qian.qing@aliyun.com>
2015-12-26 09:03:28 +0800

    add client web

--- client/js/controllers/product.js
+++ client/js/controllers/product.js
@@ -1,40 +1,39 @@
 angular
-    .module('app')
-    .controller('ProductController', ['$scope', 'Product',
-        function ($scope, Product) {
-            $scope.products = Product.find({
-                filter: {}
-            }, function () {
-                setInterval(function () {
-                    for (var i = 0; i < $scope.products.length; i++) {
-                        var product = $scope.products[i];
-                        var t = new Date(product.secKillEnd);//取得指定时间的总毫秒数
-                        var n = new Date().getTime(),//取得当前毫秒数
-                            c = t - n;//得到时间差
-                        if (c >= 0) {
-                            product.remainTime = getRemainTime(c);
-                        } else {
-                            product.remainTime = '过期了';
-                        }
-                        $scope.$digest();
-                    }
-                }, 1000);
+  .module('app')
+  .controller('ProductController', ['$scope', 'Product',
+    function ($scope, Product) {
+      $scope.products = Product.find({
+        filter: {}
+      });
 
-            });
+      setInterval(function () {
+        for (var i = 0; i < $scope.products.length; i++) {
+          var product = $scope.products[i];
+          var t = new Date(product.secKillEnd);//取得指定时间的总毫秒数
+          var n = new Date().getTime(),//取得当前毫秒数
+            c = t - n;//得到时间差
+          if (c >= 0) {
+            product.remainTime = getRemainTime(c);
+          } else {
+            product.remainTime = '过期了';
+          }
+        }
+        $scope.$digest();
+      }, 100);
 
-            function getRemainTime(sec) {
-                var days = Math.floor(sec / (1000 * 60 * 60 * 24));
-                var hours = Math.floor(sec / (1000 * 60 * 60)) % 24;
-                var minutes = Math.floor(sec / (1000 * 60)) % 60;
-                var seconds = Math.floor(sec / 1000) % 60;
-                if (days < 0)
-                    days = 0;
-                if (hours < 0)
-                    hours = 0;
-                if (minutes < 0)
-                    minutes = 0;
-                if (seconds < 0)
-                    seconds = 0;
-                return days+"天"+hours+"小时"+minutes+"分"+seconds+"秒";
-            }
-        }]);
+      function getRemainTime(sec) {
+        var days = Math.floor(sec / (1000 * 60 * 60 * 24));
+        var hours = Math.floor(sec / (1000 * 60 * 60)) % 24;
+        var minutes = Math.floor(sec / (1000 * 60)) % 60;
+        var seconds = Math.floor(sec / 1000) % 60;
+        if (days < 0)
+          days = 0;
+        if (hours < 0)
+          hours = 0;
+        if (minutes < 0)
+          minutes = 0;
+        if (seconds < 0)
+          seconds = 0;
+        return days + "天" + hours + "小时" + minutes + "分" + seconds + "秒";
+      }
+    }]);
--- server/boot/create-sample-data.js
+++ server/boot/create-sample-data.js
@@ -5,39 +5,41 @@ var async = require('async');
 var utils = require('../util/utils');
 
 module.exports = function (app) {
-    // create all models
-    async.parallel({
-        users: async.apply(createUsers),
-        products: async.apply(createProducts)
-    }, function(err, results) {
-        if (err) throw err;
+  // create all models
+  async.parallel({
+    users: async.apply(createUsers),
+    products: async.apply(createProducts)
+  }, function (err, results) {
+    if (err) throw err;
 
-        console.log('> models created successfully');
-        console.log('> create ' + results.users.length +' user');
-        console.log('> create ' + results.products.length +' product');
-    });
+    console.log('> models created successfully');
+    console.log('> create ' + results.users.length + ' user');
+    console.log('> create ' + results.products.length + ' product');
+  });
 
-    // create user
-    function createUsers(cb) {
-        var users = [];
-        for (var i = 0; i < 3; i++) {
-            var emailStr = 'qianqing'+ i + '@aliyun.com';
-            users[i] = {email: emailStr, password: '123'};
-        }
-        app.models.MYUser.create(users, cb);
+  // create user
+  function createUsers(cb) {
+    var users = [];
+    for (var i = 0; i < 3; i++) {
+      var emailStr = 'qianqing' + i + '@aliyun.com';
+      users[i] = {email: emailStr, password: '123'};
     }
+    app.models.MYUser.create(users, cb);
+  }
 
-    // create product
-    function createProducts(cb) {
-        var products = [];
-        for (var i = 0; i < 3; i++) {
-            var productName = '商品'+ i;
-            var secKillStart = new Date();
-            var secKillEnd = new Date();
-            secKillEnd.setMinutes(secKillStart.getMinutes() + 1);
-            products[i] = {name: productName, count: 1000 + i, isSecKill: true,
-                secKillStart: utils.formatTime(secKillStart), secKillEnd: utils.formatTime(secKillEnd)};
-        }
-        app.models.Product.create(products, cb);
+  // create product
+  function createProducts(cb) {
+    var products = [];
+    for (var i = 0; i < 3; i++) {
+      var productName = '商品' + i;
+      var secKillStart = new Date();
+      var secKillEnd = new Date();
+      secKillEnd.setMinutes(secKillStart.getMinutes() + 1);
+      products[i] = {
+        name: productName, count: 1000 + i, isSecKill: true,
+        secKillStart: utils.formatTime(secKillStart), secKillEnd: utils.formatTime(secKillEnd)
+      };
     }
+    app.models.Product.create(products, cb);
+  }
 };
