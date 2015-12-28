3bb3b2abc1ec8d6ba9ece55090b87f9f892f39a7
qianqing <qian.qing@aliyun.com>
2015-12-28 13:10:32 +0800

    modify order logic

--- common/models/product.js
+++ common/models/product.js
@@ -1,3 +1,36 @@
 module.exports = function(Product) {
+  Product.getProjects = function(cb) {
+    Product.getApp(function (err , app) {
+      var errStr = JSON.stringify({status:0, msg:'异常错误'});
+      if (err) {
+        cb(null, errStr);
+      } else {
+        app.models.Product.find({}, function (err, projects) {
+          if (err) {
+            cb(null, errStr);
+            return;
+          }
 
+          if (!projects) {
+            cb(null, JSON.stringify({projects:[]}));
+            return;
+          }
+
+          if (projects.count <= 0) {
+            cb(null, JSON.stringify({projects:[]}));
+            return;
+          }
+
+          cb(null, JSON.stringify({projects:projects}));
+        });
+      }
+    });
+  };
+  Product.remoteMethod(
+    'getProjects',
+    {
+      returns: {arg: 'projects', type: 'string'},
+      http: {path:'/get-products', verb: 'get'}
+    }
+  );
 };
